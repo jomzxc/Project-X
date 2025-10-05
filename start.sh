10597105c59174ef4593decc7231793876ac087c
jomzxc <202210892@fit.edu.ph>
2025-10-06 04:10:10 +0800

    fix start.sh to use absolute paths for tailscaled and tailscale commands

--- start.sh
+++ start.sh
@@ -3,12 +3,9 @@
 # Exit on error
 set -e
 
-# Start the Tailscale daemon in the background
-tailscaled &
+/usr/sbin/tailscaled &
 
-# Bring the Tailscale interface up and log in.
-# The TS_AUTHKEY is passed in as an environment variable from the docker run command.
-tailscale up --authkey=${TS_AUTHKEY} --hostname=tess-classifier-prod --accept-routes
+/usr/bin/tailscale up --authkey=${TS_AUTHKEY} --hostname=tess-classifier-prod --accept-routes
 
 # Start the Tailscale Funnel to expose port 8000
 tailscale funnel 8000 &
