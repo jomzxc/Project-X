3b96822dd6a0a75cdfd6099d72826b1bf15a0e45
jomzxc <202210892@fit.edu.ph>
2025-10-06 10:56:35 +0800

    refactor start.sh to use relative paths for Tailscale commands and add sleep before bringing up the interface

--- start.sh
+++ start.sh
@@ -3,13 +3,17 @@
 # Exit on error
 set -e
 
-/usr/sbin/tailscaled &
+# Start the Tailscale daemon in the background.
+tailscaled &
 
-/usr/bin/tailscale up --authkey=${TS_AUTHKEY} --hostname=tess-classifier-prod --accept-routes
+sleep 3
 
-# Start the Tailscale Funnel to expose port 8000
+# Bring the Tailscale interface up and log in.
+tailscale up --authkey=${TS_AUTHKEY} --hostname=tess-classifier-prod --accept-routes
+
+# Start the Tailscale Funnel to expose port 8000.
 tailscale funnel 8000 &
 
-# Start the Waitress server in the foreground
+# Start the Waitress server in the foreground.
 echo "Starting Waitress server..."
 waitress-serve --host=0.0.0.0 --port=8000 tess_classifier.wsgi:application
